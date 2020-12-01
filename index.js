7b64efa7f46a3216906486f4a2b1c1a047a89077
PatrickD1964 <75077831+PatrickD1964@users.noreply.github.com>
2020-12-01 11:57:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+/* NAVBAR OVERLAY */
+
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+console.log("5200");
+
+/*NAVBAR OVERLAY fin*/
+
+
+/* MODAL */
+
+var modal=document.getElementById('myModal');
+var btn=document.getElementById('myBtn');
+var span=document.getElementsByClassName('close')[0];
+btn.onclick=function(){
+    
+    modal.style.display= "block";
+    
+}
+
+span.onclick=function(){
+    
+    modal.style.display= "none";
+}
+
+window.onclick=function(){
+    
+    if(event.target= "modal"){
+        modal.style.display= "none";
+    }
+}    
+
+/* MODAL FIN  */
+
+/*  POPOVER */
+
+   $(function(){
+      $(".myPopover").popover();
+   });
+
+/*TOOLTIP */
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+
